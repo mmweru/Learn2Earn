b5c34acfbb4104c46255c296d3c0fa05afabd22d
Maryann Mweru <mwerumaryann@gmail.com>
2024-12-16 15:29:37 +0300

    feat: Remove of the footer and login buttons

--- splash-card-swiper/src/components/Navigation.jsx
+++ splash-card-swiper/src/components/Navigation.jsx
@@ -36,11 +36,6 @@ const Navigation = () => {
                     ))
                 }
             </ul>
-            {/* signup and login btn*/}
-            <div className='text-base text-primary font-medium space-x-5 hidden lg:block'>
-                <Link to="/login" className="py-2 px-5 border rounded">Log in</Link>
-                <Link to="/sign-up" className="py-2 px-5 border rounded bg-blue-700 text-white">Sign up</Link>
-            </div>
             {/* mobile menu*/}
             <div className='md:hidden block'>
                <button onClick={handleMenuToggler}>
